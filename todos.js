c5a5dd965e620f83acf6608a42d169a27f38bc22
Brynja Palina Sigurgeirsdottir <binnapalla@gmail.com>
2023-10-31 12:46:25 +0000

    modified my code after comparing to solution

--- todos.js
+++ todos.js
@@ -7,7 +7,6 @@ while (command !== "quit") {
         let newTodo = prompt("Enter new todo");
         todos.push(newTodo);
         console.log(`${newTodo} added to list`);
-        command = prompt("What would you like to do?");
     }
     else if (command === "list") {
         console.log("************");
@@ -15,20 +14,16 @@ while (command !== "quit") {
             console.log(`${i}: ${todos[i]}`);
         }
         console.log("************");
-        command = prompt("What would you like to do?");
     }
     else if (command === "delete") {
-        let deleteTodo = prompt("Enter index of todo to delete")
-        while (deleteTodo >= todos.length) {
+        let deleteTodo = parseInt(prompt("Enter index of todo to delete"));
+        while (deleteTodo >= todos.length || Number.isNaN(deleteTodo)) {
             deleteTodo = prompt("Invalid index. Enter index of todo to delete");
         }
         todos.splice(deleteTodo, 1);
         console.log("Todo Removed");
-        command = prompt("What would you like to do?");
     }
-    else {
-        command = prompt("What would you like to do?");
-    }
+    command = prompt("What would you like to do?");
 }
 
 console.log("OK, YOU QUIT THE APP");
